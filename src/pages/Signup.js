38855b866b28ca10cdbf1deaa194a240fc68cf38
Sid-Thakur <tsidharthsingh@gmail.com>
2025-06-03 16:37:26 +0530

    Update Signup.js

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -36,7 +36,7 @@ function Signup() {
       return handleError("name ,email & password are required");
     }
     try {
-      const url = "http://localhost:8080/auth/signup";
+      const url = "https://fin-app-backend.vercel.app/auth/signup";
       const response = await fetch(url, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
